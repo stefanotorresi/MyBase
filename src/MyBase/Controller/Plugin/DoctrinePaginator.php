12c07453575a5634df4c3d41bcb9c31548ec5012
Stefano Torresi <webdev@stefanotorresi.it>
2013-04-22 16:59:56 +0200

    add DoctrinePaginator controller plugin

---
+++ src/MyBase/Controller/Plugin/DoctrinePaginator.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright (c) 2013 Stefano Torresi (http://stefanotorresi.it)
+ * See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace MyBase\Controller\Plugin;
+
+use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrinePaginatorAdapter;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
+use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+use Zend\Paginator\Paginator;
+
+class DoctrinePaginator extends AbstractPlugin
+{
+    /**
+     *
+     * @param  string        $entityClass
+     * @param  mixed         $query       Entity FQCN or Doctrine query or Doctrine query builder.
+     * @return ZendPaginator
+     */
+    public function __invoke($data, $itemCountPerPage = 10)
+    {
+        if ($data instanceof EntityRepository) {
+            $query = $data->createQueryBuilder('query');
+        } else {
+            $query = $data;
+        }
+
+        $ormPaginator = new ORMPaginator($query);
+        $adapter = new DoctrinePaginatorAdapter($ormPaginator);
+        $paginator = new Paginator($adapter);
+        $paginator->setDefaultItemCountPerPage($itemCountPerPage);
+
+        $page = (int) $this->getController()->params()->fromQuery('page');
+
+        if ($page) {
+            $paginator->setCurrentPageNumber($page);
+        }
+
+        return $paginator;
+    }
+
+}
