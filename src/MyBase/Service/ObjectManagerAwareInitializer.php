515fe2da84c64a6c1e230924e325991f023df024
Stefano Torresi <webdev@stefanotorresi.it>
2013-04-24 19:18:37 +0200

    add ObjectManager initializer

---
+++ src/MyBase/Service/ObjectManagerAwareInitializer.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright (c) 2013 Stefano Torresi (http://stefanotorresi.it)
+ * See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace MyBase\Service;
+
+use Doctrine\ORM\EntityManager;
+use DoctrineModule\Persistence\ObjectManagerAwareInterface;
+use Zend\ServiceManager\InitializerInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\AbstractPluginManager;
+
+class ObjectManagerAwareInitializer implements InitializerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize($instance, ServiceLocatorInterface $serviceLocator)
+    {
+        if ( ! $instance instanceof ObjectManagerAwareInterface ) {
+            return;
+        }
+
+        $serviceManager = ( $serviceLocator instanceof AbstractPluginManager ) ?
+            $serviceLocator->getServiceLocator () : $serviceLocator;
+
+        /* @var $objectManager EntityManager */
+        $objectManager = $serviceManager->get('Doctrine\ORM\EntityManager');
+
+        $instance->setObjectManager($objectManager);
+    }
+
+}
