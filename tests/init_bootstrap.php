8ed35afc3fa306f85f12208c40f1320c722bade8
Stefano Torresi <webdev@stefanotorresi.it>
2013-10-17 17:19:54 +0200

    streamlining test boostrap

---
+++ tests/init_bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace MyBaseTest;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+include __DIR__ . '/' . __NAMESPACE__ . '/Bootstrap.php';
+
+Bootstrap::init();
